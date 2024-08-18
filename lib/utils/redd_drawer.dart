06930019652945731f83d74944c842fc31265abf
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2024-08-18 09:41:53 +0200

    redd drawer update

--- lib/utils/redd_drawer.dart
+++ lib/utils/redd_drawer.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ReddDrawer extends StatelessWidget {
+  const ReddDrawer({super.key});
+
+  // Common button style for all buttons in the drawer
   ButtonStyle buttonStyle() {
-    return ButtonStyle(
-        padding: const WidgetStatePropertyAll(EdgeInsets.all(8)),
-        foregroundColor: WidgetStateColor.resolveWith((_) {
-          return const Color.fromARGB(255, 255, 255, 255);
-        }),
-        backgroundColor: WidgetStateColor.resolveWith((_) {
-          return const Color.fromARGB(118, 217, 1, 255);
-        }));
+    return ElevatedButton.styleFrom(
+      padding: EdgeInsets.zero,
+      foregroundColor: Colors.white,
+      backgroundColor: Color.fromARGB(255, 10, 6, 11),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      fixedSize: const Size(60, 60),
+    );
   }
 
   static const sizedBoxHeight = SizedBox(
     height: 25,
   );
-
-  const ReddDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,40 +29,45 @@ class ReddDrawer extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          ElevatedButton(
-              style: buttonStyle(),
-              child: const Text('Call Me'),
-              onPressed: () {
-                launchUrl(Uri.parse('tel:+263788793302'),
-                    mode: LaunchMode.externalApplication);
-              }),
-          sizedBoxHeight,
-          ElevatedButton(
-              style: buttonStyle(),
-              child: const Text('Contact Me On Email'),
-              onPressed: () {
-                launchUrl(
-                    Uri.parse(
-                        'mailto: luphahlablessingthamsanqa@gmail.com?subject=Business&body=Hey Dynamic Digital Design can you'),
-                    mode: LaunchMode.externalApplication);
-              }),
-          sizedBoxHeight,
-          ElevatedButton(
-              style: buttonStyle(),
-              child: const Text('Connect With Me On Whatsapp'),
-              onPressed: () {
-                launchUrl(
-                    Uri.parse('https://wa.me/263788793302?text=Hey REDD AXE'),
-                    mode: LaunchMode.externalApplication);
-              }),
-          sizedBoxHeight,
           const Text(
-            'contact the developer',
+            'Contact Developer',
             style: TextStyle(
-                color: Colors.purple,
-                fontWeight: FontWeight.w500,
-                fontSize: 15),
-          )
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: 15,
+            ),
+          ),
+          sizedBoxHeight,
+          ElevatedButton(
+            style: buttonStyle(),
+            child: const Icon(Icons.phone, size: 20),
+            onPressed: () {
+              launchUrl(Uri.parse('tel:+263788793302'),
+                  mode: LaunchMode.externalApplication);
+            },
+          ),
+          sizedBoxHeight,
+          ElevatedButton(
+            style: buttonStyle(),
+            child: const Icon(Icons.email, size: 20),
+            onPressed: () {
+              launchUrl(
+                  Uri.parse(
+                      'mailto: luphahlablessingthamsanqa@gmail.com?subject=Business&body=Hey Dynamic Digital Design can you'),
+                  mode: LaunchMode.externalApplication);
+            },
+          ),
+          sizedBoxHeight,
+          ElevatedButton(
+            style: buttonStyle(),
+            child: const FaIcon(FontAwesomeIcons.whatsapp,
+                size: 20), // WhatsApp icon from FontAwesome
+            onPressed: () {
+              launchUrl(
+                  Uri.parse('https://wa.me/263788793302?text=Hey REDD AXE'),
+                  mode: LaunchMode.externalApplication);
+            },
+          ),
         ],
       ),
     );
