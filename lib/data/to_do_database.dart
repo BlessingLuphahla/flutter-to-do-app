43a06c9a630b444989f76bd74a46d02dde981a5b
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2024-09-17 00:30:57 +0200

    Update to_do_database.dart

--- lib/data/to_do_database.dart
+++ lib/data/to_do_database.dart
@@ -5,8 +5,6 @@ class ToDoDatabase {
 
   final _toDoBox = Hive.box('ToDoBox');
 
-  // run this function if its first time ever opening this app
-
   void createInitialData() {
     toDoList = [
       ["Slide A Task For More Options", false],
